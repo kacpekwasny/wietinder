9dda7fa9e5acdd73d053350dd2f5cf37f4a4a214
Natabyc <nataliabyc01@gmail.com>
2023-07-19 09:05:56 +0200

    vuetify installed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <v-app>
     <v-main>
-      <router-view/>
+      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
+  components: {
+    HelloWorld,
+  },
 
   data: () => ({
     //
