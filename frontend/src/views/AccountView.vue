896cbcc6f7321fb5982cf60c9b4fd79f15684317
Kacper Kwaśny <kwasny.it@gmail.com>
2023-07-20 18:12:56 +0200

    moja plec, pozadana plec

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -1,4 +1,5 @@
-<script>
+<script lang="ts">
+
 export default {
 
   data() {
@@ -7,10 +8,13 @@ export default {
       imageView: null,
       description: '',
       targetSex: [],
+      mySex: "",
+      overlay: false,
     };
   },
+  
   methods: {
-    handleFileUpload(event) {
+    handleFileUpload(event: Event) {
       this.file = event.target.files[0];
     },
     submitForm() {
@@ -35,24 +39,43 @@ export default {
 <!-- zrobic zeby zdjecia sie wyswietlaly, check box kogo szukam, piwo/etc, nie widac boxów :(()) -->
 
 <template>
-  <v-layout>
-    <v-form>
-      <v-text-field label="Opis" v-model="description"></v-text-field>
+    <v-container class="d-flex flex-column">
+      <v-textarea label="Opis profilu" v-model="description"></v-textarea>
       <v-file-input multiple accept="image/png, image/jpeg" label="Dodaj zdjęcia" v-model="file"
         @change="handleFileUpload"></v-file-input>
-      <button @click="submitForm">Upload</button>
 
-      <v-card>
-        <v-card-title>
-          Płeć pary:
-        </v-card-title>
-        <v-card-action>
-          <v-checkbox v-model="targetSex" label="Mężczyzna" value="male"></v-checkbox>
-          <v-checkbox v-model="targetSex" label="Kobieta" value="female"></v-checkbox>
+      <v-container fluid>
+        <v-row dense>
+          <v-col>
+            <v-card class="align-end" height="100%">
+              <v-card-title class="text-left">
+                Moja płeć:
+              </v-card-title>
+              <v-card-action>
+                <v-radio-group v-model="mySex" column>
+                  <v-radio label="Kobieta" color="blue" value="female" density="default"></v-radio>
+                  <v-radio label="Mężczyzna" color="blue" value="male" density="default"></v-radio>
+                </v-radio-group>
+              </v-card-action>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card class="align-end" height="100%">
+              <v-card-title class="text-left">
+                Pożądana płeć pary:
+              </v-card-title>
+              <v-card-action>
+                <v-checkbox v-model="targetSex" label="Kobieta" value="female" hideDetails density="compact" class="ml-2"></v-checkbox>
+                <v-checkbox v-model="targetSex" label="Mężczyzna" value="male" hideDetails density="compact" class="ml-2"></v-checkbox>
+              </v-card-action>
+            </v-card>
 
-        </v-card-action>
-      </v-card>
-    </v-form>
-  </v-layout>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <v-btn @click="submitForm">Zapisz zmiany!</v-btn>
+
+    </v-container>
 </template>
   
