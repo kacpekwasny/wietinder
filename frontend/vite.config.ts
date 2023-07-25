f75fc27725ee3fed4119ab433dd29406716c8e75
Kacper Kwaśny <kwasny.it@gmail.com>
2023-07-25 18:42:57 +0200

    fix envDir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: '../',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
