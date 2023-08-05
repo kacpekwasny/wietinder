826b0acf4c38448b2aceb29daab7fafe37db9b08
Kacper Kwaśny <kwasny.it@gmail.com>
2023-08-05 19:50:54 +0200

    header style max width 800px

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,4 +1,7 @@
 <template>
-  <v-card-title class="text-h3">WIETINDER</v-card-title>
+  <v-container class="d-flex" style="max-width: 800px;">
+    <v-card-title class="text-h3" >WIETINDER</v-card-title>
+
+  </v-container>
 </template>
 
