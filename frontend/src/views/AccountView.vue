fe213a3d3841d4e5bc4ea5225b0c1af2c4e67f59
Kacper Kwaśny <kwasny.it@gmail.com>
2023-08-05 19:55:13 +0200

    max 9 images. max 2MB

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -1,4 +1,5 @@
 <script lang="ts">
+import { file } from "@babel/types";
 import draggable from "vuedraggable";
 
 export default {
@@ -10,6 +11,19 @@ export default {
       targetSex: [],
       mySex: "",
       overlay: false,
+      rules: [
+        (files: File[]) => {
+          if (files.length > 9) {
+            return "Można dodać maksymalnie 9 zdjęć."
+          }
+          for (let f of files) {
+            if (f.size > 2_000_000) {
+              return `Images should be smaller than 2MB. ${f.name} has size: ${(f.size/1_000_000).toFixed(2)} MB.`;
+            }
+          }
+        }
+        
+      ]
     };
   },
 
@@ -36,7 +50,7 @@ export default {
     },
     makeURL(file: File) {
       return URL.createObjectURL(file);
-    }
+    },
   },
 
   components: {
@@ -48,42 +62,41 @@ export default {
 <!-- zrobic zeby zdjecia sie wyswietlaly, check box kogo szukam, piwo/etc, nie widac boxów :(()) -->
 
 <template>
-  <v-container class="d-flex flex-column">
-    <v-textarea label="Opis profilu" v-model="description"></v-textarea>
-    <v-file-input
-      chips
-      multiple
-      prepend-icon="mdi-camera"
-      accept="image/*"
-      label="Dodaj zdjęcia"
-      v-model="selectedImages"
-      @change="addImagesChanged"
-    ></v-file-input>
+  <v-container fluid class="d-flex flex-column" style="max-width: 800px;">
+      <v-textarea label="Opis profilu" v-model="description"></v-textarea>
+      <v-file-input
+        chips
+        multiple
+        :rules="rules"
+        prepend-icon="mdi-camera"
+        accept="image/*"
+        label="Dodaj zdjęcia"
+        v-model="selectedImages"
+        @change="addImagesChanged"
+      ></v-file-input>
 
-    <draggable
-      v-model="selectedImages"
-      item-key="url"
-      group="people"
-      style="align-items: center; display: flex; flex-wrap: wrap"
-    >
-    <!-- Had to mannulay add these style elements copied from https://github.com/vuetifyjs/vuetify/blob/master/packages/vuetify/src/components/VGrid/VGrid.sass
+      <draggable
+        v-model="selectedImages"
+        item-key="url"
+        group="people"
+        style="align-items: center; display: flex; flex-wrap: wrap"
+      >
+        <!-- Had to mannulay add these style elements copied from https://github.com/vuetifyjs/vuetify/blob/master/packages/vuetify/src/components/VGrid/VGrid.sass
     for the column layout to start working, because when v-row was the parent of draggable,
     draggable became a column of width 2/12 I think, and as such its children became even thinnger :/ -->
-      <template #item="{ element: preview }">
-        <v-col cols="6" sm="4" md="3" lg="2">
-          <v-card class="ma-4">
-            <v-img :src="makeURL(preview)" height="125"></v-img>
-            <v-card-text class="text-caption">
-              {{ preview.name }}
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </template>
-    </draggable>
-
-    <v-container fluid>
+        <template #item="{ element: preview }">
+          <v-col cols="6" sm="4" class="pa-2">
+            <v-card density="compact">
+              <v-img :src="makeURL(preview)" max-height="150px"></v-img>
+              <div class="text-caption ma-1">
+                {{ preview.name }}
+              </div>
+            </v-card>
+          </v-col>
+        </template>
+      </draggable>
       <v-row dense>
-        <v-col>
+        <v-col col="6">
           <v-card class="align-end" height="100%">
             <v-card-title class="text-left"> Moja płeć: </v-card-title>
             <v-card-action>
@@ -104,7 +117,7 @@ export default {
             </v-card-action>
           </v-card>
         </v-col>
-        <v-col>
+        <v-col col="6">
           <v-card class="align-end" height="100%">
             <v-card-title class="text-left"> Pożądana płeć pary: </v-card-title>
             <v-card-action>
@@ -128,8 +141,7 @@ export default {
           </v-card>
         </v-col>
       </v-row>
+      <v-btn class="mt-2 mb-2" @click="submitForm">Zapisz zmiany!</v-btn>
     </v-container>
 
-    <v-btn @click="submitForm">Zapisz zmiany!</v-btn>
-  </v-container>
 </template>
