afae30efb14f47ae0d9074f9e6548ccdc91cee42
Kacper Kwaśny <kwasny.it@gmail.com>
2023-09-07 01:33:14 +0200

    display messages in Chat

--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -1,35 +1,31 @@
-<script lang = "ts">
-import { useChatsListPanelStore } from "@/stores/ChatsListPanelStore"
-import { useChatsStore } from "@/stores/ChatsStore"
-import { getBackendHostname } from "@/common/requests"
-
+<script lang="ts">
+import { useChatsListPanelStore } from "@/stores/ChatsListPanelStore";
+import { useChatsStore } from "@/stores/ChatsStore";
+import { getBackendHostname } from "@/common/requests";
 
 export default {
-    data() {
-        return {
-          chatsStore: useChatsStore(),
-          ChatsListPanelStore: useChatsListPanelStore(),
-        }
-    },
-    
-    methods: {
-        toggleChatsListsPanel(){
-            this.ChatsListPanelStore.toggleChatsListPanel()
-        },
+  data() {
+    return {
+      chatsStore: useChatsStore(),
+      ChatsListPanelStore: useChatsListPanelStore(),
+    };
+  },
 
-        remoteURL(imageName: string) {
-            return `${getBackendHostname()}/uploads/${imageName}`;
-        },
+  methods: {
+    toggleChatsListsPanel() {
+      this.ChatsListPanelStore.toggleChatsListPanel();
     },
-}
 
+    remoteURL(imageName: string) {
+      return `${getBackendHostname()}/uploads/${imageName}`;
+    },
+  },
+};
 </script>
 
 <template>
-   <v-card class="chat-card">
-    <v-card-title
-    v-if="chatsStore.activeChat != null"
-    class="pa-2 d-flex justify-between align-center">
+  <v-card class="chat-card" v-if="chatsStore.activeChat != null">
+    <v-card-title class="pa-2 d-flex justify-between align-center">
       <v-btn @click="toggleChatsListsPanel" icon class="ml-3 mr-5">
         <v-icon>mdi-comment-outline</v-icon>
       </v-btn>
@@ -37,5 +33,15 @@ export default {
         {{ chatsStore.activeChat.profile.name }}
       </div>
     </v-card-title>
+    <v-card-content>
+      <v-list>
+        <v-list-item v-for="msg in chatsStore.activeChat.messages">
+          <v-card class="ma-3 pa-2 elevation-3">
+            <p>{{ msg.author }}</p>
+            <p>{{ msg.message }}</p>
+          </v-card>
+        </v-list-item>
+      </v-list>
+    </v-card-content>
   </v-card>
 </template>
--- frontend/src/stores/ChatsStore.ts
+++ frontend/src/stores/ChatsStore.ts
@@ -14,12 +14,13 @@ export const useChatsStore = defineStore('ChatsStore', {
     },
     actions: {
         async fetchChats() {
-            const resp = await getJson('/chats') // To chyba nie powinno dawać ID użytkowników tylko liste Chatów
+            const resp = await getJson('/chats')
             const chats: Chat[] = await resp.json()
             chats.sort((ch1, ch2) => (ch1.messages[0].datetime + ch2.messages[0].datetime))
             chats.forEach(ch => {
                 this._chats.set(ch.profile.public_id, ch)
             })
+            this.activeChat = chats[0]
         },
 
         toTopOfChatOrder(publicId: string) {
