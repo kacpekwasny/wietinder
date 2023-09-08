d65ab9f59430a672b9637f841b0060fcb5d628c6
Kacper Kwaśny <kwasny.it@gmail.com>
2023-09-09 00:22:32 +0200

    cleanup + receive profiles, not only publicIds

--- frontend/src/views/ChatsView.vue
+++ frontend/src/views/ChatsView.vue
@@ -2,39 +2,21 @@
 import { getJson } from "../common/requests";
 import { useProfilesStore } from "@/stores/ProfilesStore";
 import Chat from "@/components/Chat.vue";
+import { useChatsStore } from "@/stores/ChatsStore";
 
 
 export default {
     data() {
         return {
-            matchesID: [] as string[],
             profilesStore: useProfilesStore(),
-            profileData: {
-                name: "" as string,
-                images: [] as string[],
-                bio: "" as string,
-                sex: "" as string,
-                fields_of_study: [] as string[],
-                target_sex: [] as string[],
-                target_activity: [] as string[],
-            },
+            chatStore: useChatsStore(),
         };
     },
     methods: {
-        async loadData() {
-            let resp = await getJson("/my-matches");
-            let json = await resp.json();
-            this.matchesID = json;
-        },
-        async getUserData(userID: string) {
-            const p = await this.profilesStore.profile(userID);
-            if (p === undefined) {
-                this.profileData.name = "404, profile not found";
-                this.profileData.bio = "profile with this ID was not found";
-                return;
-            }
-            this.profileData = p;
-        },
+    },
+    async created() {
+        await this.chatStore.fetchChats()
+        this.chatStore.makeActive(this.chatStore.toList()[0].profile.public_id)
     },
     components: { Chat }
 }
--- frontend/src/views/LikesMeView.vue
+++ frontend/src/views/LikesMeView.vue
@@ -1,52 +1,29 @@
 <script lang="ts">
 import {getJson} from "../common/requests";
-import { useProfilesStore } from "@/stores/ProfilesStore";
+import { Profile, useProfilesStore } from "@/stores/ProfilesStore";
 
 export default {
     data() {
         return {
           profilesStore: useProfilesStore(),
-          usersWhoLikesMe: [],
-          profileData: {
-              name: "" as string,
-              images: [] as string[],
-              bio: "" as string,
-              sex: "" as string,
-              fields_of_study: [] as string[],
-              target_sex: [] as string[],
-              target_activity: [] as string[],
-          },
-          profilesData: [],
+          profilesData: [] as Profile[],
         }
     },
     methods: {
-        async loadData(){
-            this.profilesData = [];
+        async fetchWhoLikesMe(){
             let response = await getJson("/who-likes-me")
-            let json =  await response.json();
-            this.usersWhoLikesMe = json
+            let profilesThatLikeMe: Profile[] =  await response.json();
+            this.profilesData = profilesThatLikeMe
+            this.profilesStore.addProfilesToCache(profilesThatLikeMe)
+        },
 
-            for(let user of this.usersWhoLikesMe){
-              await this.getUserData(user);
-              this.profilesData.push(this.profileData)
-            }  
-        },
-        async getUserData(userID: string){
-          const p = await this.profilesStore.profile(userID);
-            if (p === undefined) {
-                this.profileData.name = "404, profile not found";
-                this.profileData.bio = "profile with this ID was not found";
-                return;
-            }
-            this.profileData = p;
-        },
         navigateToAccount(userID: string){
           this.$router.push({ path: "/profile/" + userID})
         },
     },
 
-    created(){
-        this.loadData();
+    created() {
+        this.fetchWhoLikesMe();
     }
 }
 
@@ -60,7 +37,7 @@ export default {
       <div class="text-h5 mb-5 mt-3 text-center">Zobacz kto cię polubił!</div>
       <v-row class="pa-2">
         <v-col
-            v-for="(profile, index) in profilesData"
+            v-for="profile in profilesData"
               cols="6"
               sm="4"
               class="pa-1"
