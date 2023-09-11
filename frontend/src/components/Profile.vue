38273aefb509ef890e48c7f12bb7a85f2a59c0c4
Kacper Kwaśny <kwasny.it@gmail.com>
2023-09-11 22:22:47 +0200

    tried to fix carousel; but with no success

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -51,50 +51,34 @@ export default {
       ];
       return items;
     },
-    shouldShowArrows(){
+    shouldShowArrows() {
       return this.profileData.images.length > 1 ? true : false;
     },
-    
-   
   },
-  methods:{
+  methods: {
     backendHostname() {
       return getBackendHostname();
     },
+    
     remoteURL(imageName: string) {
       return `${getBackendHostname()}/uploads/${imageName}`;
     },
-   
-  }
+  },
 };
 </script>
 
 <template>
   <v-container fluid class="d-flex flex-column" style="max-width: 800px">
-    <v-card class="pa-2 mb-4 elevation-6">
+    <v-card class="pa-2 mb-4 elevation-6" style="position: relative">
       <v-carousel hide-delimiters :show-arrows="shouldShowArrows">
-        <v-carousel-item
-        v-for="(image) in profileData.images"
-        :key="image"
-        :src="remoteURL(image)"
-        eager
-        >
-        <v-img
-          :src="remoteURL(image)"
-          max-height="480px"
-          class="align-end"
-          eager
-
-          > 
-          <div class="text-h4">{{ profileData.name }}</div>
-          <div class="text-caption">{{ profileData.bio }}</div>
-        </v-img>
-
+        <v-carousel-item v-for="image in profileData.images" :key="image" eager>
+          <v-img :src="remoteURL(image)" height="100%" eager></v-img>
         </v-carousel-item>
-        
       </v-carousel>
-      
-       
+      <div class="pa-2" style="position: absolute; bottom: 0; left: 0">
+        <div class="text-h4">{{ profileData.name }}</div>
+        <div class="text-caption">{{ profileData.bio }}</div>
+      </div>
     </v-card>
     <v-expansion-panels>
       <v-expansion-panel>
@@ -115,6 +99,5 @@ export default {
         </v-table>
       </v-expansion-panel>
     </v-expansion-panels>
-
   </v-container>
 </template>
--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -1,18 +1,17 @@
 <script lang="ts">
-import AccountSettings from '@/components/AccountSettings.vue';
-import Profile from '@/components/Profile.vue'
+import AccountSettings from "@/components/AccountSettings.vue";
+import Profile from "@/components/Profile.vue";
 import { useProfilesStore } from "@/stores/ProfilesStore";
-import { useUserAccountStore } from '@/stores/AccountDataStore';
-
+import { useUserAccountStore } from "@/stores/AccountDataStore";
 
 export default {
   data() {
-    return{
+    return {
       showProfile: false as boolean,
 
       profilesStore: useProfilesStore(),
       userAccountStore: useUserAccountStore(),
-      
+
       profileData: {
         name: "",
         images: [],
@@ -22,28 +21,29 @@ export default {
         target_sex: [],
         target_activity: [],
       },
-    }
+    };
   },
 
   methods: {
-    changeView(){
-      this.showProfile = !this.showProfile
-      
+    changeView() {
+      this.showProfile = !this.showProfile;
     },
 
     async getProfileData() {
-      const p = await this.profilesStore.profile(this.userAccountStore.accountData.public_id)
+      const p = await this.profilesStore.profile(
+        this.userAccountStore.accountData.public_id
+      );
       if (p === undefined) {
-        this.profileData.name = "404, profile not found"
-        this.profileData.bio = "profile with this ID was not found"
-        return
+        this.profileData.name = "404, profile not found";
+        this.profileData.bio = "profile with this ID was not found";
+        return;
       }
-      this.profileData = p
-    }
+      this.profileData = p;
+    },
   },
 
-  created() {
-    this.getProfileData();
+  async created() {
+    await this.getProfileData();
   },
 
   components: {
@@ -57,13 +57,13 @@ export default {
 
 <template>
   <v-btn
-  icon
-  class="v-btn--floating-action v-btn--fab v-btn--fixed ml-2 "
-  color="yellow"
-  @click = "changeView"
-  style="position:fixed, relative; bottom: 10px; z-index: 999;"
+    icon
+    class="v-btn--floating-action v-btn--fab v-btn--fixed ml-2"
+    color="yellow"
+    @click="changeView"
+    style="position: fixed, relative; bottom: 10px; z-index: 999"
   >
-    <v-icon v-if = "showProfile">mdi-account-cog</v-icon>
+    <v-icon v-if="showProfile">mdi-account-cog</v-icon>
     <v-icon v-else>mdi-account</v-icon>
   </v-btn>
 
@@ -72,4 +72,3 @@ export default {
     <AccountSettings v-else></AccountSettings>
   </v-container>
 </template>
-
