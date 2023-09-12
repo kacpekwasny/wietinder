302d98ba640bf1c18a8a75b11a06e8b7a2ff5036
Dawid <dburda@student.agh.edu.pl>
2023-09-12 19:00:13 +0200

    Profil vue images in progress

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -18,7 +18,11 @@ export default {
   },
 
   data() {
-    return {};
+    return {
+      imageDialog: false,
+      currentImageIndex: null,
+      imageToShow: "",
+    };
   },
 
   async created() {},
@@ -63,18 +67,19 @@ export default {
     remoteURL(imageName: string) {
       return `${getBackendHostname()}/uploads/${imageName}`;
     },
-  },
+    openImageDialog(image) {
+      this.imageToShow = image;
+      this.imageDialog = true;
+    }
+  }
 };
 </script>
 
 <template>
   <v-container fluid class="d-flex flex-column" style="max-width: 800px">
     <v-card class="pa-2 mb-4 elevation-6" style="position: relative">
-      <v-carousel hide-delimiters :show-arrows="shouldShowArrows">
-        <v-carousel-item v-for="image in profileData.images" :key="image" eager>
-          <v-img :src="remoteURL(image)" height="100%" eager></v-img>
-        </v-carousel-item>
-      </v-carousel>
+      
+      <v-img :src="remoteURL(profileData.images[0])" max-height="500px" eager></v-img>
       <div class="pa-2" style="position: absolute; bottom: 0; left: 0">
         <div class="text-h4">{{ profileData.name }}</div>
         <div class="text-caption">{{ profileData.bio }}</div>
@@ -99,5 +104,30 @@ export default {
         </v-table>
       </v-expansion-panel>
     </v-expansion-panels>
+    <v-card>
+      <v-card-title>Zdjęcia</v-card-title>
+      <v-row class="pa-1">
+        <v-col
+          v-for="(image, index) in profileData.images"
+          cols="6"
+          sm="4"
+          class="pa-1"
+        >
+        <v-card @click="openImageDialog(image)" class="cursor-pointer">
+            <v-img :src="remoteURL(image)" max-height="150px"></v-img>
+        </v-card>
+        <v-dialog v-model="imageDialog" max-width="600">
+          <template v-slot:activator="{ on }">
+            <!-- Clickable thumbnail image -->
+            
+          </template>
+          <v-card>
+            <!-- Image displayed in the dialog -->
+            <v-img :src="remoteURL(imageToShow)" max-height="600px"></v-img>
+          </v-card>
+        </v-dialog>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
